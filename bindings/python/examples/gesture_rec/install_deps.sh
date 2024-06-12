e9a13233eb1807936cef2113edc1fdc2b872c464
Dan Nechita <dan.nechita@analog.com>
2024-06-12 14:38:57 +0300

    Strip project and keep only the SDK
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- bindings/python/examples/gesture_rec/install_deps.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-
-sudo apt-get install -y python3-tk
-sudo apt-get install -y python3-matplotlib
-sudo apt-get install -y python3-skimage
-sudo apt-get install -y python3-pil.imagetk
-sudo apt-get install -y python3-pil
